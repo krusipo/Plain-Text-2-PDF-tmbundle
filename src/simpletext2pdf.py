23b4c31557c4f0f6c14af4407db3efd3d5d31049
krusipo <jonas@mactop.alfazero.lan>
2010-12-25 14:12:25 +0100

    syntax structure

--- src/simpletext2pdf.py
+++ src/simpletext2pdf.py
@@ -7,14 +7,37 @@ __author__ = ""
 __data__ = ""
 
 class MetaData(object):
-	def __init__(self):
+	def __init__(self, atoc = True,):
 		self.author = None
 		self.title = None
-		self.data = None
+		self.date = None
 		self.version = None
-		self.toc = True
+		self.toc = atoc
 		self.endnotes = True
 		self.path = None
+		
+class Template(object):
+	def __init__(self):
+		self._head = '<!DOCTYPE HTML><html><head><title>Simple Syntax Document</title>\
+						<meta charset="utf-8" /><style type="text/css"></style></head><body>'
+		self._end = '</body></html>'
+		
+		self.meta = MetaData()
+		self.simple_text = None
+		self.header = None
+		self.footer = None
+		
+		
+	def appendTo(tag):
+		pass
+		
+	def toString(self):
+		pass
+		
+	def toUnicodeString(self):
+		pass
+
+# ///-----Global functions.
 
 def plain_to_hmtl(plain_file):
 	pass
@@ -23,6 +46,8 @@ def join_string(str_list):
 	pass
 
 def get_file_content(_file):
+	filetype = {'md' : markdown,
+	}
 	pass
 	
 def usage():
@@ -34,7 +59,7 @@ def usage():
 
 if __name__ == "__main__":
 	try:
-		opts, args = getopt.gnu_getopt(sys.argv[1:], "hfcto", ["help", "file=", "css=","toc","open="])
+		opts, args = getopt.gnu_getopt(sys.argv[1:], "hf:c:", ["help", "file=", "css="])
 	except getopt.GetoptError, err:
 		print str(err) 
 		usage()
