0d1d0e2238acc333615f87798394ddbedb11a4b1
krusipo <jonas@mactop.alfazero.lan>
2010-12-26 12:06:27 +0100

    MetaData class done

--- src/simpletext2pdf.py
+++ src/simpletext2pdf.py
@@ -1,28 +1,58 @@
 import os
 import sys
 import getopt
+import os.path
 
-__version__ = ""
-__author__ = ""
-__data__ = ""
+__version__ = "0.3"
+__author__ = "Jonas Odencrants"
+__date__ = ""
 
 class MetaData(object):
-	def __init__(self, atoc = True,):
-		self.author = None
-		self.title = None
-		self.date = None
-		self.version = None
-		self.toc = atoc
-		self.endnotes = True
-		self.path = None
+	def __init__(self, plain_file):
 		
+		for key, value in self.file_vars(plain_file).iteritems():
+			self.__dict__[key] = value
+
+		
+	def file_vars(self, plain_file):
+		
+		local_vars = open(plain_file, 'r')
+		simple_vars = local_vars.readline()
+		file_meta = {}
+		file_meta["toc"] = True
+		file_meta["author"] = ''
+		file_meta["version"] = ''
+		file_meta["title"] = 'README'
+		file_meta["date"] = self.getDate()
+		file_meta["toc"] = True
+		try:
+			simple_vars.index('<!--Simple')
+			simple_vars = simple_vars.replace('<!--Simple ', '').replace(' -->', '')
+			temp = [s for s in simple_vars.split(';') if s]
+			for item in temp:
+			    key,value = item.split(':')
+			    file_meta[key] = value
+		except ValueError:
+			print("Meta must be on first line and like this <!--Simple author:The Author; -->")
+		
+		finally:
+			file_meta["dir"] = os.path.dirname(plain_file)
+			file_meta["file_name"] = os.path.basename(plain_file)
+			
+			
+		return file_meta
+		
+	def getDate(self):
+		from datetime import date
+		return date.today()
+		 
 class Template(object):
 	def __init__(self):
 		self._head = '<!DOCTYPE HTML><html><head><title>Simple Syntax Document</title>\
 						<meta charset="utf-8" /><style type="text/css"></style></head><body>'
 		self._end = '</body></html>'
 		
-		self.meta = MetaData()
+		self.meta = None
 		self.simple_text = None
 		self.header = None
 		self.footer = None
@@ -40,14 +70,21 @@ class Template(object):
 # ///-----Global functions.
 
 def plain_to_hmtl(plain_file):
-	pass
+	plain_type = {".md" : markdown,
+				".MD" : markdown,
+	}
+	
+	return plain_type[os.path.splitext(plain_file)](plain_file)
+
+def markdown(plain_file):
+	import markdown2
+	extras=["code-color","footnotes"]
+	return markdown2.markdown_path(plain_file, extras)
 	
 def join_string(str_list):
 	pass
 
 def get_file_content(_file):
-	filetype = {'md' : markdown,
-	}
 	pass
 	
 def usage():
@@ -66,7 +103,20 @@ if __name__ == "__main__":
 		sys.exit(2)
 	
 	for opt, arg in opts:
-		pass
+		if opt in ("-h", "--help"):
+			usage()
+			sys.exit()
+		elif opt in ("-f", "--file"):
+			# File object to get plain-text syntax from.
+				m = MetaData(arg)
+				print(m.toc)
+		elif opt in ("-c", "--css"):
+			# Use specified css file.
+			pass
+		else:
+			assert False, "unhandled option"
+	
+	
 		
 		 
 		
